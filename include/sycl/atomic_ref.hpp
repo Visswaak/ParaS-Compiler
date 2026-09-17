7e3efea529152d379935d177df206243d4182b75
Visswaak Reddy <visswaak1729@gmail.com>
2026-09-17 12:18:49 +0000

    Fix host atomic fetch_add for floating point

--- include/sycl/atomic_ref.hpp
+++ include/sycl/atomic_ref.hpp
@@ -67,6 +67,17 @@ inline constexpr std::memory_order to_std(memory_order o) {
   }
   return std::memory_order_seq_cst;
 }
+
+inline constexpr std::memory_order to_std_failure(memory_order o) {
+  switch (o) {
+  case memory_order::release:
+    return std::memory_order_relaxed;
+  case memory_order::acq_rel:
+    return std::memory_order_acquire;
+  default:
+    return to_std(o);
+  }
+}
 #endif
 
 template <typename T, memory_order DefaultOrder = memory_order::relaxed,
@@ -134,7 +145,15 @@ public:
 #if PARAS_GPU_BACKEND
     return atomicAdd(ptr, v);
 #else
-    return atomic_ptr()->fetch_add(v, to_std(o));
+    const std::memory_order success_order = to_std(o);
+    const std::memory_order failure_order = to_std_failure(o);
+    T old = atomic_ptr()->load(success_order);
+    T desired;
+    do {
+      desired = static_cast<T>(old + v);
+    } while (!atomic_ptr()->compare_exchange_weak(
+        old, desired, success_order, failure_order));
+    return old;
 #endif
   }
 
@@ -144,7 +163,15 @@ public:
 #if PARAS_GPU_BACKEND
     return atomicAdd(ptr, -v);
 #else
-    return atomic_ptr()->fetch_sub(v, to_std(o));
+    const std::memory_order success_order = to_std(o);
+    const std::memory_order failure_order = to_std_failure(o);
+    T old = atomic_ptr()->load(success_order);
+    T desired;
+    do {
+      desired = static_cast<T>(old - v);
+    } while (!atomic_ptr()->compare_exchange_weak(
+        old, desired, success_order, failure_order));
+    return old;
 #endif
   }
 
